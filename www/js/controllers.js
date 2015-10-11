1c49b3a02bd3cd189ce6774adfb46302536f5416
Marco Araujo <marco.araujo.junior@gmail.com>
2015-10-11 17:31:10 -0300

    Redesenhando tela de operação

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,6 +24,8 @@ angular.module('simplefinancial.controllers', [])
 
 .controller('OperacaoCtrl', function($scope, $state, $stateParams, Financas) {
 
+    $scope.classErro = 'oculta';
+
     if (typeof $stateParams.financaId != "undefined") {
         $scope.financa = Financas.get($stateParams.financaId);
         $scope.titulo = $scope.financa.name;
@@ -32,13 +34,9 @@ angular.module('simplefinancial.controllers', [])
         $scope.titulo = 'Nova Transação';
     }
 
-    $scope.typeList = [
-        { text: "Despesa", value: "despesa" },
-        { text: "Receita", value: "receita" },
-    ];
-
     $scope.erros = [];
     $scope.adicionaItem = function (financa, financaForm) {
+        $scope.erros = [];
         if (financaForm.date.$error.required) {
             $scope.erros.push({campo: 'Data', descricao: 'Campo obrigatório'});
         }
@@ -54,12 +52,23 @@ angular.module('simplefinancial.controllers', [])
         if (financaForm.name.$error.minlength) {
             $scope.erros.push({campo: 'Nome', descricao: 'Muito Curto'});
         }
+        financa.type = financa.type.toLowerCase();
+
+        if ($scope.erros.length != 0) {
+            $scope.classErro = 'exibe';
+        }
 
         if (financaForm.$valid) {
             Financas.save(financa);
             $state.go('tab.dash');
         }
     };
+})
+
+.filter('capitalize', function() {
+    return function(input) {
+      return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+    }
 })
 
 .controller('FinancaCtrl', function($scope, $state, $stateParams, Financas) {
